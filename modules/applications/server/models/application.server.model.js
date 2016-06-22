35d271471627545c5fb274a419cd289d5c3d7146
Sara <dic15seg@student.lu.se>
2016-06-22 15:26:48 +0200

    Ändrat i modellen vilken information som ska finnas i Application-schemat för databasen

--- modules/applications/server/models/application.server.model.js
+++ modules/applications/server/models/application.server.model.js
@@ -10,12 +10,6 @@ var mongoose = require('mongoose'),
  * Application Schema
  */
 var ApplicationSchema = new Schema({
-  name: {
-    type: String,
-    default: '',
-    required: 'Please fill Application name',
-    trim: true
-  },
   created: {
     type: Date,
     default: Date.now
@@ -23,6 +17,27 @@ var ApplicationSchema = new Schema({
   user: {
     type: Schema.ObjectId,
     ref: 'User'
+  },
+  companies: {
+    type: [String],
+    required: 'Välj minst ett företag som du vill söka kontaktsamtal med.'
+  },
+  resume: {
+    type: String,
+    required: 'Bifoga ditt CV.'
+  },
+  phone: {
+    type: String,
+    required: 'Ange ditt telefonnummer.'
+  },
+  email: {
+    type: String,
+    required: 'Ange din emailadress.'
+    //Hämta denna information från användaren??
+  },
+  times: {
+    type: [String],
+    required: 'Ange de tider du kan.'
   }
 });
 
