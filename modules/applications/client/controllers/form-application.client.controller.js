c88b1088d9036bd0fd2a1da630cafb88691ae226
Sara <dic15seg@student.lu.se>
2016-06-23 16:46:11 +0200

    Problem med drop-down

---
+++ modules/applications/client/controllers/form-application.client.controller.js
@@ -0,0 +1,16 @@
+(function(angular) {
+  'use strict';
+
+  angular
+	.module('ngrepeatSelect', [])
+  	.controller('ExampleController', ['$scope', function($scope) {
+	    $scope.data = {
+	     repeatSelect: null,
+	     availableOptions: [
+	       {id: '1', name: 'Option A'},
+	       {id: '2', name: 'Option B'},
+	       {id: '3', name: 'Option C'}
+	     ],
+	    };
+ }]);
+})(window.angular);
