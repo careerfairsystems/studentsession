015d7a35984c3f92ffef792127f8aa61ecb8b430
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-07-20 19:33:44 +0200

    try to fix indention errors... not succeed

--- modules/applications/client/controllers/applications.client.controller.js
+++ modules/applications/client/controllers/applications.client.controller.js
@@ -61,7 +61,7 @@
     // Resets the upload as unsuccessful 
     $scope.unsuccess = function () {
       $scope.success = false;
-    }
+    };
 
     // Save Application
     function save(isValid) {
@@ -92,8 +92,8 @@
 
     // Create file uploader instance
     $scope.uploader = new FileUploader({
-        url: 'api/applications/resume/' + prettify($scope.user.displayName) + $scope.user._id + '_cv' + '.pdf', //osäker på om .pdf behövs
-        alias: 'newResume'
+      url: 'api/applications/resume/' + prettify($scope.user.displayName) + $scope.user._id + '_cv' + '.pdf', //osäker på om .pdf behövs
+      alias: 'newResume'
     });
 
     function prettify(str) {
@@ -107,27 +107,27 @@
     }
 
      // Set file uploader pdf filter
-     $scope.uploader.filters.push({
-      name: 'pdfFilter',
-      fn: function (item, options) {
-        var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
-        return '|pdf|'.indexOf(type) !== -1;
-      }
-    });
+    $scope.uploader.filters.push({
+     name: 'pdfFilter',
+     fn: function (item, options) {
+      var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+      return '|pdf|'.indexOf(type) !== -1;
+    }
+   });
 
      // Called after the user selected a file
-     $scope.uploader.onAfterAddingFile = function (fileItem) {
-      if ($window.FileReader) {
-        var fileReader = new FileReader() ;
-        fileReader.readAsDataURL(fileItem._file);
+   $scope.uploader.onAfterAddingFile = function (fileItem) {
+    if ($window.FileReader) {
+      var fileReader = new FileReader() ;
+      fileReader.readAsDataURL(fileItem._file);
 
-        fileReader.onload = function (fileReaderEvent) {
-          $timeout(function () {
-            $scope.pdfURL = fileReaderEvent.target.result;
-          }, 0);
-        };
-      }
-    };
+      fileReader.onload = function (fileReaderEvent) {
+        $timeout(function () {
+          $scope.pdfURL = fileReaderEvent.target.result;
+        }, 0);
+      };
+    }
+   };
 
      // Called after the user has successfully uploaded a new resume
      $scope.uploader.onSuccessItem = function (fileItem, response, status, headers) {
