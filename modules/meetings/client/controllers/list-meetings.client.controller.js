38f16ebb731be1e84d2058fd4fa5e82a650f5fc4
Sara <dic15seg@student.lu.se>
2016-07-22 18:16:43 +0200

    Val av lokaler med checkboxes

--- modules/meetings/client/controllers/list-meetings.client.controller.js
+++ modules/meetings/client/controllers/list-meetings.client.controller.js
@@ -5,15 +5,16 @@
     .module('meetings')
     .controller('MeetingsListController', MeetingsListController);
 
-  MeetingsListController.$inject = ['MeetingsService', 'meetingResolve', 'listFacilitiesResolve', 'listApplicationsResolve', 'listCompaniesResolve'];
+  MeetingsListController.$inject = ['$scope', 'MeetingsService', 'meetingResolve', 'listFacilitiesResolve', 'listApplicationsResolve', 'listCompaniesResolve'];
 
-  function MeetingsListController(MeetingsService, meeting, facilities, applications, companies) {
+  function MeetingsListController($scope, MeetingsService, meeting, facilities, applications, companies) {
     var vm = this;
 
     vm.meetings = MeetingsService.query();
     vm.facilities = facilities;
     vm.applications = applications;
     vm.companies = companies;
+
 
     // Examples of variables that are useful;
     vm.startDate = "2016-11-16";
@@ -23,7 +24,16 @@
     vm.meetingTimeLength = "";
     //TODO: Implement method to add many meetings with some coinstraints.
 
+    $scope.chosenFacilities = {};
 
+    // takes the output from checkboxes and puts the values of the checked elements from a checkbox into an array
+    $scope.chosenFacilitiesArray = function(checked){  
+      var array=[];
 
-  }
-})();
+      angular.forEach(checked,function(key,value){
+        if(key)
+           array.push(value)
+      })
+      return array;
+    };
+}})();
