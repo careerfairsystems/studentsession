e0b9ca68482dc20db4847017d45f1e2869669e9f
Sara <dic15seg@student.lu.se>
2016-07-23 17:41:12 +0200

    Basic formulär för indata. Tider visas tyvärr med sekund och standard är nuvarande tid för närvarande, har inte ännu hittat en lösning

--- modules/meetings/client/controllers/list-meetings.client.controller.js
+++ modules/meetings/client/controllers/list-meetings.client.controller.js
@@ -15,16 +15,34 @@
     vm.applications = applications;
     vm.companies = companies;
 
-
     // Examples of variables that are useful;
-    vm.startDate = "2016-11-16";
-    vm.endDate = "2016-11-16";
-    vm.startHours = "";
-    vm.endHours = "";
-    vm.meetingTimeLength = "";
+    vm.startDate = new Date();
+    vm.endDate = new Date();
+    vm.startHours = new Date();
+    vm.endHours = new Date();
+    vm.meetingTimeLength = '';
+    vm.lunchStart = new Date();
+    vm.lunchEnd = new Date();
+    vm.chosenFacilities = {};
     //TODO: Implement method to add many meetings with some coinstraints.
 
-    $scope.chosenFacilities = {};
+    /*getCurrentDate = function() {
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth()+1; //January is 0!
+      var yyyy = today.getFullYear();
+
+      if(dd<10) {
+          dd='0'+dd
+      } 
+
+      if(mm<10) {
+          mm='0'+mm
+      } 
+
+      return (today = yyyy+ '-' + mm + '-' + dd);
+    }*/
+
 
     // takes the output from checkboxes and puts the values of the checked elements from a checkbox into an array
     $scope.chosenFacilitiesArray = function(checked){  
@@ -32,8 +50,14 @@
 
       angular.forEach(checked,function(key,value){
         if(key)
-           array.push(value)
-      })
+           array.push(value);
+      });
       return array;
+    };
+
+
+    $scope.createMeetings = function() {
+      console.log('Att göra: skapa möten');
+      //Att göra: skapa önskade möten
     };
 }})();
