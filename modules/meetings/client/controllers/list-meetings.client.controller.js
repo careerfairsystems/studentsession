cda954bd476db6ef7bfa9274ea0738901ab8d815
Sara <dic15seg@student.lu.se>
2016-07-29 13:11:12 +0200

    Datum visar nu valt datum och inte dagen innan

--- modules/meetings/client/controllers/list-meetings.client.controller.js
+++ modules/meetings/client/controllers/list-meetings.client.controller.js
@@ -3,7 +3,28 @@
 
   angular
     .module('meetings')
-    .controller('MeetingsListController', MeetingsListController);
+    .controller('MeetingsListController', MeetingsListController)
+    .directive('datepickerLocaldate', ['$parse', function ($parse) {
+     var directive = {
+       restrict: 'A',
+       require: ['ngModel'],
+       link: link
+     };
+     return directive;
+
+     function link(scope, element, attr, ctrls) {
+       var ngModelController = ctrls[0];
+
+         // called with a JavaScript Date object when picked from the datepicker
+       ngModelController.$parsers.push(function (viewValue) {
+         console.log(viewValue);console.log(viewValue);console.log(viewValue);
+             // undo the timezone adjustment we did during the formatting
+         viewValue.setMinutes(viewValue.getMinutes() - viewValue.getTimezoneOffset());
+             // we just want a local date in ISO format
+         return viewValue.toISOString().substring(0, 10);
+       });
+     }
+    }]);
 
   MeetingsListController.$inject = ['$scope', 'MeetingsService', 'meetingResolve', 'listFacilitiesResolve', 'listApplicationsResolve', 'listCompaniesResolve'];
 
@@ -26,24 +47,6 @@
     vm.chosenFacilities = {};
     //TODO: Implement method to add many meetings with some coinstraints.
 
-    /*getCurrentDate = function() {
-      var today = new Date();
-      var dd = today.getDate();
-      var mm = today.getMonth()+1; //January is 0!
-      var yyyy = today.getFullYear();
-
-      if(dd<10) {
-          dd='0'+dd
-      } 
-
-      if(mm<10) {
-          mm='0'+mm
-      } 
-
-      return (today = yyyy+ '-' + mm + '-' + dd);
-    }*/
-
-
     // puts the chosen facilities into an array
     var chosenFacilitiesArray = function(){  
       var array=[];
@@ -58,7 +61,7 @@
 
     $scope.createMeetings = function() {
         var facilitiesArray = chosenFacilitiesArray();
-        if(!!vm.startDate && !!vm.endDate && !!vm.startHours && !!vm.endHours && !!vm.lunchStart && !!vm.lunchEnd && facilitiesArray !== 'undefined' && vm.meetingTimeLength !== "" && facilitiesArray.length !== 0) {
+        if(!!vm.startDate && !!vm.endDate && !!vm.startHours && !!vm.endHours && !!vm.lunchStart && !!vm.lunchEnd && facilitiesArray !== 'undefined' && vm.meetingTimeLength !== '' && facilitiesArray.length !== 0) {
           console.log('Alla fält är ifyllda');
         } else {
           console.log('Alla fält är inte ifyllda!');
