7104cf0bdfdac557f94edde4daaf14c62ed2967a
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-08-31 01:15:56 +0200

    install datatables

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,7 +5,8 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/datatables.net-bs/css/dataTables.bootstrap.min.css'
       ],
       js: [
         'public/lib/angular/angular.js',
@@ -16,7 +17,10 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/datatables.net/js/jquery.dataTables.min.js',
+        'public/lib/datatables.net-bs/js/dataTables.bootstrap.min.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
