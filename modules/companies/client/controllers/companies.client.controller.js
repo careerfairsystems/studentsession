fcb6a98bcb06146d188821d484e92a6b5470a445
NilssonJohan <johan.skolarbeten@gmail.com>
2016-08-30 15:43:39 +0200

    Fixed logo view

--- modules/companies/client/controllers/companies.client.controller.js
+++ modules/companies/client/controllers/companies.client.controller.js
@@ -17,72 +17,84 @@
     vm.form = {};
     vm.remove = remove;
     vm.save = save;
-    vm.imageURL = '/api/companies/picture/' + vm.company.profileImageURL;
+    vm.imageURL = '';
+
+    $scope.file_changed = function(element) {
+      var photofile = element.files[0];
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $scope.$apply(function() {
+          $scope.imageURL = e.target.result;
+          vm.company.profileImageURL = $scope.imageURL;
+        });
+      };
+      reader.readAsDataURL(photofile);
+    };
 
    // Create file uploader instance
-    $scope.uploader = new FileUploader({
-      url: 'api/companies/picture',
-      alias: 'newProfilePicture'
-    });
+//    $scope.uploader = new FileUploader({
+//      url: 'api/companies/picture',
+//      alias: 'newProfilePicture'
+//    });
 
    // Set file uploader image filter
-    $scope.uploader.filters.push({
-      name: 'imageFilter',
-      fn: function (item, options) {
-        var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
-        return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
-      }
-    });
+//  $scope.uploader.filters.push({
+//      name: 'imageFilter',
+//      fn: function (item, options) {
+//        var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+//        return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+//      }
+//    });
 
    // Called after the user selected a new picture file
-    $scope.uploader.onAfterAddingFile = function (fileItem) {
-      if ($window.FileReader) {
-        var fileReader = new FileReader();
-        fileReader.readAsDataURL(fileItem._file);
+  //  $scope.uploader.onAfterAddingFile = function (fileItem) {
+  //    if ($window.FileReader) {
+  //      var fileReader = new FileReader();
+  //      fileReader.readAsDataURL(fileItem._file);
 
-        fileReader.onload = function (fileReaderEvent) {
-          $timeout(function () {
-            $scope.imageURL = fileReaderEvent.target.result;
-          }, 0);
-        };
-      }
-    };
+  //      fileReader.onload = function (fileReaderEvent) {
+  //        $timeout(function () {
+  //          $scope.imageURL = fileReaderEvent.target.result;
+  //        }, 0);
+  //      };
+  //    }
+  //  };
 
    // Called after the user has successfully uploaded a new picture
-    $scope.uploader.onSuccessItem = function (fileItem, response, status, headers) {
+    //$scope.uploader.onSuccessItem = function (fileItem, response, status, headers) {
       // Show success message
-      $scope.success = true;
+    //  $scope.success = true;
 
      // Populate user object
-      $scope.company = response;
+    //  $scope.company = response;
 
      // Clear upload buttons
-      $scope.cancelUpload();
-    };
+    //  $scope.cancelUpload();
+    //};
 
    // Called after the user has failed to uploaded a new picture
-    $scope.uploader.onErrorItem = function (fileItem, response, status, headers) {
+    //$scope.uploader.onErrorItem = function (fileItem, response, status, headers) {
       // Clear upload buttons
-      $scope.cancelUpload();
+    //  $scope.cancelUpload();
 
       // Show error message
-      $scope.error = response.message;
-    };
+    //  $scope.error = response.message;
+    //};
 
    // Change user profile picture
-    $scope.uploadProfilePicture = function () {
+    //$scope.uploadProfilePicture = function () {
       // Clear messages
-      $scope.success = $scope.error = null;
+    //  $scope.success = $scope.error = null;
 
      // Start upload
       //$scope.uploader.uploadAll();
-    };
+    //};
 
    // Cancel the upload process
-    $scope.cancelUpload = function () {
-      $scope.uploader.clearQueue();
-      $scope.imageURL = '/api/companies/picture/' + vm.company.profileImageURL;
-    };
+  // $scope.cancelUpload = function () {
+  //    $scope.uploader.clearQueue();
+  //    $scope.imageURL = '/api/companies/picture/' + vm.company.profileImageURL;
+  //  };
 
 
     // Remove existing Company
@@ -98,8 +110,6 @@
         $scope.$broadcast('show-errors-check-validity', 'vm.form.companyForm');
         return false;
       }
-
-      vm.company.profileImageURL = $scope.imageURL;
 
       // TODO: move create/update logic to service
       if (vm.company._id) {
