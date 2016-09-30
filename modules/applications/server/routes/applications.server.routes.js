947986dc5c1f616f27fab290f8a72da4456cba58
Sara <dic15seg@student.lu.se>
2016-09-30 15:52:05 +0200

    mergefix

--- modules/applications/server/routes/applications.server.routes.js
+++ modules/applications/server/routes/applications.server.routes.js
@@ -8,15 +8,10 @@ var applicationsPolicy = require('../policies/applications.server.policy'),
 
 module.exports = function(app) {
   // Applications Routes
-<<<<<<< HEAD
   app.route('/api/applications/confirmationmail')
     .post(applications.confirmationMail);
 
-  app.route('/api/applications')
-    .post(applications.create);
-=======
   app.route('/api/applications').post(applications.create);
->>>>>>> guestAllowedApplication
 
   app.route('/api/applications').all(applicationsPolicy.isAllowed)
     .get(applications.list);
