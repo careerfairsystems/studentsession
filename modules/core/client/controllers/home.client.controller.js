ea54e55ea071253b12e469a8510e3ef6b0883ade
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-10-03 01:58:55 +0200

    make frontpage pretty

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -9,7 +9,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     $scope.gotoSelected = function() {
       // set the location.hash to the id of
       // the element you wish to scroll to.
-      $location.hash('companyname');
+      $location.hash('company');
 
       // call $anchorScroll()
       $anchorScroll();
