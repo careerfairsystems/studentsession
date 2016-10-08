f80ec22ebfc10f39bf68b1b730b7c0223b09870b
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-10-08 19:10:20 +0200

    add delete all meetings feature

--- modules/meetings/client/controllers/list-meetings.client.controller.js
+++ modules/meetings/client/controllers/list-meetings.client.controller.js
@@ -11,5 +11,16 @@
     var vm = this;
  
     vm.meetings = MeetingsService.query();
+
+    vm.deleteAllMeetings = function (){
+      
+      function deleteMeeting(m){
+        var meeting = MeetingsService.get({ meetingId: m._id }, function() {
+          meeting.$delete();
+        });
+      }
+      vm.meetings.forEach(deleteMeeting);
+      vm.meetings = [];
+    };
   }
 })();
