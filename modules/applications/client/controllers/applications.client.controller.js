09bb684f57ba0fb1933dbaade90ce1d732078f15
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2016-10-19 12:57:22 +0200

    extract programs to service

--- modules/applications/client/controllers/applications.client.controller.js
+++ modules/applications/client/controllers/applications.client.controller.js
@@ -8,11 +8,11 @@
   .controller('ApplicationsController', ApplicationsController);
 
   ApplicationsController.$inject = ['$scope', '$state', '$timeout', '$window', 'Authentication', 'FileUploader',
-  'applicationResolve', 'CompaniesService'];
+  'applicationResolve', 'CompaniesService', 'ProgramsService'];
 
 
   function ApplicationsController ($scope, $state, $timeout, $window, Authentication, FileUploader,
-    application, CompaniesService) {
+    application, CompaniesService, ProgramsService) {
     var vm = this;
 
     vm.authentication = Authentication;
@@ -93,52 +93,8 @@
       { time: 14, available: false },
     ];
     //programs
-    var allPrograms = ['Byggteknik med arkitektur / Civil Engineering - Architecture',
-                  'Civilingenjör i Riskhantering / Master of Science in Engineering, Risk Management and Safety Engineering',
-                  'Arkitekt / Architect',
-                  'Arkitekt / Architect',
-                  'Medicin och teknik / Biomedical Engineering',
-                  'Bioteknik / Biotechnology',
-                  'Bioteknik / Biotechnology',
-                  'Kemiteknik / Chemical Engineering',
-                  'Kemiteknik / Chemical Engineering',
-                  'Brandingenjörsutbildning / Fire Protection Engineering',
-                  'Brandingenjörsutbildning / Fire Protection Engineering',
-                  'Byggteknik med arkitektur / Civil Engineering - Architecture',
-                  'Byggteknik med järnvägsteknik / Civil Engineering - Railway Construction',
-                  'Byggteknik med järnvägsteknik / Civil Engineering - Railway Construction',
-                  'Civil Engineering- Road and Traffic Technology / Civil Engineering- Road and Traffic Technology',
-                  'Civil Engineering- Road and Traffic Technology / Civil Engineering- Road and Traffic Technology',
-                  'Väg- och vattenbyggnad / Civil Engineering',
-                  'Väg- och vattenbyggnad / Civil Engineering',
-                  'Datateknik / Computer Science and Engineering',
-                  'Datateknik / Computer Science and Engineering',
-                  'Informations- och kommunikationsteknik / Information and Communication Engineering',
-                  'Informations- och kommunikationsteknik / Information and Communication Engineering',
-                  'Ekosystemteknik / Environmental Engineering',
-                  'Elektroteknik / Electrical Engineering',
-                  'Elektroteknik / Electrical Engineering',
-                  'Teknisk Matematik / Engineering Mathematics',
-                  'Teknisk Nanovetenskap / Engineering Nanoscience',
-                  'Teknisk Fysik / Engineering Physics',
-                  'Teknisk Matematik / Engineering Mathematics',
-                  'Teknisk Fysik / Engineering Physics',
-                  'Teknisk Nanovetenskap / Engineering Nanoscience',
-                  'Ekosystemteknik / Environmental Engineering',
-                  'Industridesign / Industrial Design',
-                  'Industriell ekonomi / Industrial Engineering and Management',
-                  'Industridesign / Industrial Design',
-                  'Industriell ekonomi / Industrial Engineering and Management',
-                  'Lantmäteri / Surveying',
-                  'Maskinteknik med teknisk design / Mechanical Engineering with Industrial Design',
-                  'Maskinteknik med teknisk design / Mechanical Engineering with Industrial Design',
-                  'Maskinteknik / Mechanical Engineering',
-                  'Maskinteknik / Mechanical Engineering',
-                  'Medicin och teknik / Biomedical Engineering',
-                  'Lantmäteri / Surveying'];
-
-    var set = new Set(allPrograms);
-    $scope.programs = Array.from(set);
+    var programsSet = new Set(ProgramsService);
+    $scope.programs = Array.from(programsSet);
 
     $scope.years = [1, 2, 3, 4, 5];
 
